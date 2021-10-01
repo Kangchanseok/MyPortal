9f519f2505dfaa9211faa538ad246d75b84da502
강찬석 <ssimjunlke@naver.com>
2021-10-01 11:41:11 +0900

    이메일 중복체크를 위한 자바스크립트 추가

--- myportal/src/main/java/com/bitacademy/myportal/controllers/UsersController.java
+++ myportal/src/main/java/com/bitacademy/myportal/controllers/UsersController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bitacademy.myportal.exception.UserDaoException;
 import com.bitacademy.myportal.repository.UserVo;
 import com.bitacademy.myportal.service.UserService;
 
@@ -29,7 +30,13 @@ public class UsersController {
 	public String joinAction(@ModelAttribute UserVo userVo) {
 		System.out.println("가입폼:" + userVo);
 		
-		boolean bSuccess = userServiceImpl.join(userVo);
+		boolean bSuccess = false;
+		try {
+		bSuccess = userServiceImpl.join(userVo);
+		} catch (UserDaoException e) {
+			System.err.println("에러 상황의 UserVo: " + userVo);
+			e.printStackTrace();
+		}
 		
 		if (bSuccess) { // 가입 성공
 			return "redirect:/users/joinsuccess";
